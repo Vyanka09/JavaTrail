bbb6b8e78820e3f492107dc7b33bb43b224c4e82
Vyanka09 <vyankateshkulkarni42@gmail.com>
2024-01-27 21:17:04 +0530

    Added exp file

---
+++ Exp.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String args[])
+    {
+    String str=new String("Hello");
+    System.out.println(str.charAt(1));
+    System.out.println(str.substring(1));
+    System.out.println(str.contains("ll"));
+}
+}
